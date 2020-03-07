f5ab050e2ebe32fdb58479282bc120fc6065376b
May Love <alove2@wisc.edu>
2020-03-07 16:45:53 -0500

    order includes review

--- cafe-angular/src/app/order.ts
+++ cafe-angular/src/app/order.ts
@@ -1,9 +1,11 @@
 import { OrderItem } from "./order-item";
 import { OrderStatus } from "./order-status.enum";
+import { Review } from './review';
 
 export class Order {
     customer: Customer;
     price: number;
     status: OrderStatus;
     orderItems: OrderItem[];
+    review: Review;
 }
