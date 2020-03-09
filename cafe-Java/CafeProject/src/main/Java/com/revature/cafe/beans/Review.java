d6a36ac0894cb6b61ea8636f1d2c5becb8b2b5f0
davidyounn <davidyounn@gmail.com>
2020-03-09 16:14:25 -0400

    did review bean in JAVA

--- cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Review.java
+++ cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Review.java
@@ -1,8 +1,28 @@
 
 package com.revature.cafe.beans;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Review {
-    private int number;
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="review")
+	@SequenceGenerator(name="review", sequenceName="review_seq", allocationSize=1)
+    private Integer id;
+    private Integer orderId;
     private boolean goodRating;
     private String comments;
+    
 }
