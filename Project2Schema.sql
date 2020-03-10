0d295ad7bd336964f1f23f978ee068f2e8c0e787
Jordan Zech <s521476@nwmissouri.edu>
2020-03-10 18:17:12 -0400

    created sequences in sql, update menuHibernate

--- Project2Schema.sql
+++ Project2Schema.sql
@@ -8,10 +8,20 @@ drop table order_item CASCADE CONSTRAINTS;
 drop table menu CASCADE CONSTRAINTS;
 drop table review CASCADE CONSTRAINTS;
 
+drop sequence cust_seq;
+drop sequence emp_seq;
+drop sequence user_seq;
+drop sequence ordS_seq;
+drop sequence add_seq;
+drop sequence ord_seq;
+drop sequence menu_seq;
+drop sequence ordI_seq;
+drop sequence rev_seq;
 --customers, menu, inventory, rewards, orders, employee, review
 
 --cust id references customer table
 --emp id references employee table
+create sequence cust_seq NOCACHE;
 
 create table cust_t(
 cust_id number(5) primary key,
@@ -20,6 +30,7 @@ cust_last varchar2(50),
 rewards_id number(5), 
 reward_stars number(5)
 );
+create sequence emp_seq NOCACHE;
 
 create table employee_t(
 emp_id number(5) primary key,
@@ -27,6 +38,7 @@ emp_first varchar2(50),
 emp_last varchar2(50)
 
 );
+create sequence user_seq NOCACHE;
 
 create table user_t(
 user_id number(5) primary key,
@@ -38,10 +50,14 @@ CONSTRAINT fk_uCustId FOREIGN KEY (cust_id) REFERENCES cust_t(cust_id),
 CONSTRAINT fk_uEmpId FOREIGN KEY (emp_id) REFERENCES employee_t(emp_id)
 );
 
+create sequence ordS_seq NOCACHE;
+
 create table order_status(
 status_id number(5) primary key,
 status varchar2(50)
 );
+
+create sequence add_seq NOCACHE;
 
 create table address(
 address_id number(5) primary key,
@@ -50,6 +66,8 @@ city varchar2(100) not null,
 states varchar2(100) not null,
 zipcode varchar2(100) not null
 );
+
+create sequence ord_seq NOCACHE;
 
 create table orders(
 order_id number(5) primary key,
@@ -64,6 +82,8 @@ CONSTRAINT fk_status FOREIGN KEY (status) REFERENCES order_status(status_id)
 
 --cust_id references customer
 --status references order_status table
+create sequence menu_seq NOCACHE;
+
 create table menu(
 menu_id number primary key,
 item_name varchar(50),
@@ -71,6 +91,7 @@ item_price decimal(10,2),
 inventory number(10,0) check (inventory > 0)
 );
 
+create sequence ordI_seq NOCACHE;
 
 create table order_item(
 item_id number(5) primary key,
@@ -81,7 +102,7 @@ CONSTRAINT fk_order_item FOREIGN KEY (order_id) REFERENCES orders(order_id),
 CONSTRAINT fk_menu_item FOREIGN KEY (menu_item) REFERENCES menu(menu_id)
 ); -- many to one relationship to an order
 
-
+create sequence rev_seq NOCACHE;
 
 create table review(
 review_id number(5) primary key,
