9810d796d8391598cdf91c7923a2bfb13871d796
davidyounn <davidyounn@gmail.com>
2020-03-10 17:43:12 -0400

    user bean

---
+++ cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/User.java
@@ -0,0 +1,10 @@
+package com.revature.cafe.beans;
+
+public class User {
+	private Integer id;
+	private Integer custId;
+	private Integer empId;
+	private String username;
+	private String password;
+
+}
