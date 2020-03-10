d40db50d92ff65f9b30fa59232a8fe326f5e62db
Alexander Bae <alexbaelafayette@gmail.com>
2020-03-10 17:28:37 -0400

    updated beans for Address Customer Employee and MenuItem

--- cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Address.java
+++ cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Address.java
@@ -1,11 +1,116 @@
 
 package com.revature.cafe.beans;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Tables;
+
+@Entity
+@Table
 public class Address {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="address")
+	@SequenceGenerator(name="address", sequenceName="address_seq", allocationSize=1)
     private int id;
     private String address;
     private String city;
     private String state;
     private String zipCode;
     
+    public Address() {
+    	super();
+    }
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (id != other.id)
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", address=" + address + ", city=" + city + ", state=" + state + ", zipCode="
+				+ zipCode + "]";
+	}
 }
--- cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Customer.java
+++ cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Customer.java
@@ -1,12 +1,115 @@
 
 package com.revature.cafe.beans;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Tables;
+
 import java.util.List;
 
+@Entity
+@Table
 public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="cust_t")
+	@SequenceGenerator(name="cust_t", sequenceName="cust_t_seq", allocationSize=1)
     private int id;
     private String first;
     private String last;
     private int stars;
     private List<Order> orders;
+    
+    public Customer() {
+    	super();
+    }
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	public int getStars() {
+		return stars;
+	}
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((last == null) ? 0 : last.hashCode());
+		result = prime * result + ((orders == null) ? 0 : orders.hashCode());
+		result = prime * result + stars;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (first == null) {
+			if (other.first != null)
+				return false;
+		} else if (!first.equals(other.first))
+			return false;
+		if (id != other.id)
+			return false;
+		if (last == null) {
+			if (other.last != null)
+				return false;
+		} else if (!last.equals(other.last))
+			return false;
+		if (orders == null) {
+			if (other.orders != null)
+				return false;
+		} else if (!orders.equals(other.orders))
+			return false;
+		if (stars != other.stars)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", first=" + first + ", last=" + last + ", stars=" + stars + ", orders=" + orders
+				+ "]";
+	}
+    
 }
