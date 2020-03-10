e1fcae8412b0fa63351983e9df80d676761e9983
Alexander Bae <alexbaelafayette@gmail.com>
2020-03-10 18:45:07 -0400

    Review (plz review code to make sure it's good

--- cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Order.java
+++ cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/Order.java
@@ -3,17 +3,129 @@ package com.revature.cafe.beans;
 
 import java.util.List;
 
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 
 public class Order {
     
     public enum OrderStatus{
         
     }
-    
+    @ManyToOne
+    @JoinColumn(name = "orders", nullable=false)
+    @Column(name = "cust_id")
     private Customer customer;
     private double price;
+    @Column(name = "status")
     private OrderStatus status;
     private List<OrderItem> orderItems;
     private Review review;
+    @Column(name = "address_id")
     private Address address;
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+	public Review getReview() {
+		return review;
+	}
+	public void setReview(Review review) {
+		this.review = review;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((orderItems == null) ? 0 : orderItems.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((review == null) ? 0 : review.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} else if (!customer.equals(other.customer))
+			return false;
+		if (orderItems == null) {
+			if (other.orderItems != null)
+				return false;
+		} else if (!orderItems.equals(other.orderItems))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (review == null) {
+			if (other.review != null)
+				return false;
+		} else if (!review.equals(other.review))
+			return false;
+		if (status != other.status)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Order [customer=" + customer + ", price=" + price + ", status=" + status + ", orderItems=" + orderItems
+				+ ", review=" + review + ", address=" + address + "]";
+	}
+    
+    
 }
