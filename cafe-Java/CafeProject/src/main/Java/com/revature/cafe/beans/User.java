f05645e892c924c1679f4492d9d970471876f8ae
davidyounn <davidyounn@gmail.com>
2020-03-10 17:49:58 -0400

    User bean with hibernate annotations

--- cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/User.java
+++ cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/User.java
@@ -1,10 +1,128 @@
 package com.revature.cafe.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="user_t")
+
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user_t")
+	@SequenceGenerator(name="user_t", sequenceName="user_seq", allocationSize=1)
+	@Column(name="user_id")
 	private Integer id;
+	@Column(name="cust_id")
 	private Integer custId;
+	@Column(name="emp_id")
 	private Integer empId;
+	@Column(name="user_name")
 	private String username;
+	@Column(name="user_pass")
 	private String password;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(Integer id, Integer custId, Integer empId, String username, String password) {
+		super();
+		this.id = id;
+		this.custId = custId;
+		this.empId = empId;
+		this.username = username;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", custId=" + custId + ", empId=" + empId + ", username=" + username + ", password="
+				+ password + "]";
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCustId() {
+		return custId;
+	}
+	public void setCustId(Integer custId) {
+		this.custId = custId;
+	}
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((custId == null) ? 0 : custId.hashCode());
+		result = prime * result + ((empId == null) ? 0 : empId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (custId == null) {
+			if (other.custId != null)
+				return false;
+		} else if (!custId.equals(other.custId))
+			return false;
+		if (empId == null) {
+			if (other.empId != null)
+				return false;
+		} else if (!empId.equals(other.empId))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+	
 
 }
