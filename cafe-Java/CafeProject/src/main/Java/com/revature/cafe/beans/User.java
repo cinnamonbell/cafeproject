a65f221f9e0c63d477a22389d420129cfbfc17db
davidyounn <davidyounn@gmail.com>
2020-03-10 18:12:42 -0400

    Fixed user table with onetoone join column

--- cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/User.java
+++ cafe-Java/CafeProject/src/main/Java/com/revature/cafe/beans/User.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -18,12 +20,15 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user_t")
 	@SequenceGenerator(name="user_t", sequenceName="user_seq", allocationSize=1)
+
 	@Column(name="user_id")
 	private Integer id;
-	@Column(name="cust_id")
-	private Integer custId;
-	@Column(name="emp_id")
-	private Integer empId;
+	@OneToOne
+	@JoinColumn(name="cust_id")
+	private Customer customer;
+	@OneToOne
+	@JoinColumn(name="emp_id")
+	private Employee employee;
 	@Column(name="user_name")
 	private String username;
 	@Column(name="user_pass")
@@ -32,55 +37,25 @@ public class User {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public User(Integer id, Integer custId, Integer empId, String username, String password) {
+	public User(Integer id, Customer customer, Employee employee, String username, String password) {
 		super();
 		this.id = id;
-		this.custId = custId;
-		this.empId = empId;
+		this.customer = customer;
+		this.employee = employee;
 		this.username = username;
 		this.password = password;
 	}
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", custId=" + custId + ", empId=" + empId + ", username=" + username + ", password="
-				+ password + "]";
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public Integer getCustId() {
-		return custId;
-	}
-	public void setCustId(Integer custId) {
-		this.custId = custId;
-	}
-	public Integer getEmpId() {
-		return empId;
-	}
-	public void setEmpId(Integer empId) {
-		this.empId = empId;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
+		return "User [id=" + id + ", customer=" + customer + ", employee=" + employee + ", username=" + username
+				+ ", password=" + password + "]";
 	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((custId == null) ? 0 : custId.hashCode());
-		result = prime * result + ((empId == null) ? 0 : empId.hashCode());
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((employee == null) ? 0 : employee.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((password == null) ? 0 : password.hashCode());
 		result = prime * result + ((username == null) ? 0 : username.hashCode());
@@ -95,15 +70,15 @@ public class User {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (custId == null) {
-			if (other.custId != null)
+		if (customer == null) {
+			if (other.customer != null)
 				return false;
-		} else if (!custId.equals(other.custId))
+		} else if (!customer.equals(other.customer))
 			return false;
-		if (empId == null) {
-			if (other.empId != null)
+		if (employee == null) {
+			if (other.employee != null)
 				return false;
-		} else if (!empId.equals(other.empId))
+		} else if (!employee.equals(other.employee))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -122,7 +97,36 @@ public class User {
 			return false;
 		return true;
 	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	
-
 }
