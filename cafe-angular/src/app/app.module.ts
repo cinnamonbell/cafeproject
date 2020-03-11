bc1294d6755154ac186f469e33e57911e0eb72c3
May Love <alove2@wisc.edu>
2020-03-11 14:24:33 -0400

    commits

--- cafe-angular/src/app/app.module.ts
+++ cafe-angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PendingOrdersComponent } from './pending-orders/pending-orders.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PendingOrdersComponent } from './pending-orders/pending-orders.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
