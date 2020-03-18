60c0a8114a154784c8c43d1c3f36ca794b48162f
unknown <kanashimidays@gmail.com>
2020-03-18 13:54:33 -0400

    added routing to view pending

--- cafe-angular/src/app/app-routing.module.ts
+++ cafe-angular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PendingOrdersComponent } from './pending-orders/pending-orders.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pending-orders', component: PendingOrdersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
