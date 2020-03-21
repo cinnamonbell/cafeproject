635373883a0c3e02beb0c38c0a88bd6d75600fb2
davidyounn <davidyounn@gmail.com>
2020-03-21 16:56:25 -0400

    navbar highlights page you're on

--- cafe-angular/src/app/nav-bar/nav-bar.component.ts
+++ cafe-angular/src/app/nav-bar/nav-bar.component.ts
@@ -38,6 +38,18 @@ export class NavBarComponent implements OnInit {
   ngOnInit(): void {
     this.loginService.getLoggedInUser().subscribe(user => { this.user = user; (user != null && user.customer != null) ? this.cust = user.customer : null; });
     console.log(this.user);
+            // Add active class to the current button (highlight it)
+            var header = document.getElementById("myDIV");
+            var btns = header.getElementsByClassName("btn");
+            console.log(btns);
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function() {
+              var current = document.getElementsByClassName("active");
+              current[0].className = current[0].className.replace(" active", "");
+              this.className += " active";
+              });
+            }
+    
   }
 
 
